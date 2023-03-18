f505a559ef1e952d43653f86e211803a07161413
DuyHung02 <duyhung23102002@gmail.com>
2023-03-19 00:27:47 +0700

    create branch hung

--- src/main/java/com/becasemodule6/config/SecurityConfig.java
+++ src/main/java/com/becasemodule6/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().ignoringAntMatchers("/**");
-        http.authorizeRequests().antMatchers( "/login/**", "/register/**","/singer/name/{singer_name}").permitAll()
+        http.authorizeRequests().antMatchers( "/login/**", "/register/**","/singer/name/{singer_name}", "/**").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable();
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
